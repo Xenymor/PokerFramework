489c7bb724aa9c357ddde20046d6035033e3eb5e
Xenymor <101249409+Xenymor@users.noreply.github.com>
2025-07-26 11:50:01 +0200

    Refactor for Use directly in Java

---
+++ src/Framework/Bots/Player.java
@@ -0,0 +1,22 @@
+package Framework.Bots;
+
+import Framework.Action;
+import Framework.Card;
+
+import java.util.List;
+
+public abstract class Player {
+    final Card[] hand;
+
+    public Player() {
+        this.hand = new Card[2];
+    }
+
+    public void newRound(final Card card1, final Card card2) {
+        this.hand[0] = card1;
+        this.hand[1] = card2;
+    }
+
+    public abstract Action takeTurn(final List<Card> cloneBoard, final int[] cloneBets, final int[] cloneStacks, final int pot, final int currPlayerIndex);
+
+}
