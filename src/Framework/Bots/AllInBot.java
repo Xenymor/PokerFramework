6be9c18ca48f46fc99e760308b5726fc9171ed8b
Xenymor <101249409+Xenymor@users.noreply.github.com>
2025-07-26 17:38:22 +0200

    Create AllInBot.java

---
+++ src/Framework/Bots/AllInBot.java
@@ -0,0 +1,13 @@
+package Framework.Bots;
+
+import Framework.Action;
+import Framework.Card;
+
+import java.util.List;
+
+public class AllInBot  extends Player {
+    @Override
+    public Action takeTurn(final List<Card> cloneBoard, final int[] cloneBets, final int[] cloneStacks, final int pot, final int currPlayerIndex) {
+        return new Action(Framework.ActionType.RAISE, cloneStacks[currPlayerIndex]);
+    }
+}
